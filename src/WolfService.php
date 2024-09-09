85fafeaf6805b18bf46e2d447444862aef87fa76
phong2018 <phong2018@gmail.com>
2024-09-10 04:43:03 +0700

    WS-002: fix ecs code

--- src/WolfService.php
+++ src/WolfService.php
@@ -11,21 +11,22 @@ final class WolfService
      */
     public function __construct(
         private array $items
-    ) { }
+    ) {
+    }
 
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Apple AirPods' and $item->name != 'Apple iPad Air') {
+            if ($item->name !== 'Apple AirPods' and $item->name !== 'Apple iPad Air') {
                 if ($item->quality > 0) {
-                    if ($item->name != 'Samsung Galaxy S23') {
+                    if ($item->name !== 'Samsung Galaxy S23') {
                         $item->quality = $item->quality - 1;
                     }
                 }
             } else {
                 if ($item->quality < 50) {
                     $item->quality = $item->quality + 1;
-                    if ($item->name == 'Apple iPad Air') {
+                    if ($item->name === 'Apple iPad Air') {
                         if ($item->sellIn < 11) {
                             if ($item->quality < 50) {
                                 $item->quality = $item->quality + 1;
@@ -40,15 +41,15 @@ final class WolfService
                 }
             }
 
-            if ($item->name != 'Samsung Galaxy S23') {
+            if ($item->name !== 'Samsung Galaxy S23') {
                 $item->sellIn = $item->sellIn - 1;
             }
 
             if ($item->sellIn < 0) {
-                if ($item->name != 'Apple AirPods') {
-                    if ($item->name != 'Apple iPad Air') {
+                if ($item->name !== 'Apple AirPods') {
+                    if ($item->name !== 'Apple iPad Air') {
                         if ($item->quality > 0) {
-                            if ($item->name != 'Samsung Galaxy S23') {
+                            if ($item->name !== 'Samsung Galaxy S23') {
                                 $item->quality = $item->quality - 1;
                             }
                         }
